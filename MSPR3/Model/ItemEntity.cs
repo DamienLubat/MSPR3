cb8c4e95ccff7299f9de406d0fcb30bb42fc655f
s2y-404 <syd95@hotmail.fr>
2023-09-06 15:26:00 +0200

    add comment for test generation

--- MSPR3/Model/ItemEntity.cs
+++ MSPR3/Model/ItemEntity.cs
@@ -2,6 +2,9 @@
 
 namespace MSPR3.Model
 {
+    /// <summary>
+    /// Classe de l'entité Item
+    /// </summary>
     public class ItemEntity
     {
         public int IDItem { get; set; }
@@ -22,6 +25,15 @@ namespace MSPR3.Model
 
         public string? Name { get; set; }
         public string? KeywordDescription { get; set; }
+
+        /// <summary>
+        /// Constructeur Items
+        /// </summary>
+        /// <param name="IDItem"></param>
+        /// <param name="GTIN"></param>
+        /// <param name="DescriptionShort"></param>
+        /// <param name="Price"></param>
+        /// <param name="MediaPath"></param>
         public ItemEntity(int IDItem, string GTIN, string DescriptionShort, decimal Price, string MediaPath)
         {
             this.IDItem = IDItem;
@@ -30,7 +42,13 @@ namespace MSPR3.Model
             this.Price = Price;
             this.MediaPath = MediaPath;
         }
-
+        /// <summary>
+        /// Constructeur Items
+        /// </summary>
+        /// <param name="IDItem"></param>
+        /// <param name="IDSupplier"></param>
+        /// <param name="Name"></param>
+        /// <param name="KeywordDescription"></param>
         public ItemEntity(int IDItem, int IDSupplier, string Name, string KeywordDescription)
         {
             this.IDItem = IDItem;
@@ -41,18 +59,34 @@ namespace MSPR3.Model
 
         public ItemEntity() { }
 
+        /// <summary>
+        /// Requête sql de création
+        /// </summary>
+        /// <returns></returns>
         public string CreatedEntity()
         {
             return "INSERT INTO Items (GTIN, ItemWeigth, SaleUnite, IDMedia, IDLanguage, IDDescriptive, IDSupplier, IDPrice, IDTax, IDVolume) VALUES (@GTIN, @ItemWeigth, @SaleUnite, @IDMedia, @IDLanguage, @IDDescriptive, @IDSupplier, @IDPrice, @IDTax, @IDVolume)";
         }
+        /// <summary>
+        /// Requête sql de lecture
+        /// </summary>
+        /// <returns></returns>
         public string ReadEntity()
         {
             return "SELECT IDItem, GTIN, ItemWeigth, SaleUnite, IDMedia, IDLanguage, IDDescriptive, IDSupplier, IDPrice, IDTax, IDVolume FROM Items WHERE IDItem = @IDItem";
         }
+        /// <summary>
+        /// Requête sql de modification
+        /// </summary>
+        /// <returns></returns>
         public string UpdateEntity()
         {
             return "UPDATE Items SET GTIN = @GTIN, ItemWeigth = @ItemWeigth, SaleUnite = @SaleUnite, IDMedia = @IDMedia, IDLanguage = @IDLanguage, IDDescripti= @IDDescriptive, IDSupplier = @IDSupplier, IDPrice = @IDPrice, IDTax = @IDTax, IDVolume = @IDVolume WHERE IDItem = @IDItem";
         }
+        /// <summary>
+        /// Requête sql de suppression
+        /// </summary>
+        /// <returns></returns>
         public string DeleteEntity()
         {
             return "DELETE FROM Items WHERE IDItem = @IDItem";
